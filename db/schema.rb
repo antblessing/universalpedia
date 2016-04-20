ef7662b416ba86dcd8f335358e70cddc6a49e806
antblessing <antblessing@gmail.com>
2016-04-20 00:22:05 -0700

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 20151117032419) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.integer  "role"
+    t.integer  "role",                   default: 0
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
